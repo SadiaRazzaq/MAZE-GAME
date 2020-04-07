66c541d89d5fc1bd1460a66161e0dc503b664ef1
SadiaRazzaq <sadiarazzaq030@gmail.com>
2020-04-07 13:01:34 -0400

    FINAL COMMIT

---
+++ Title1.py
@@ -0,0 +1,14 @@
+import turtle
+
+wn=turtle.Screen()
+wn.bgpic("TL1.png")
+wn.title("A Maze Game")
+wn.setup(700,700)
+
+def fun(x,y):
+    import Level1
+    
+wn.onclick(fun)
+
+wn.mainloop()
+    
---
+++ Title3.py
@@ -0,0 +1,14 @@
+import turtle
+
+wn=turtle.Screen()
+wn.bgpic("TL3.png")
+wn.title("A Maze Game")
+wn.setup(700,700)
+
+def fun(x,y):
+    import Level3
+    
+wn.onclick(fun)
+
+wn.mainloop()
+
